c8c61040f44fdedeb21709b206b57c2683ea43c0
Tavo Nieves J <ganieves@outlook.com>
2020-11-01 13:14:05 -0500

    Added debug dependencies

--- config/packages/dev/framework.php
+++ config/packages/dev/framework.php
@@ -6,16 +6,8 @@ use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigura
 
 return static function (ContainerConfigurator $config): void {
 
-    // Framework
-    $config->extension('framework', [
-        'test' => true,
-        'session' => [
-            'storage_id' => 'session.storage.mock_file'
-        ]
-    ]);
-
     // Web Profiler
     $config->extension('framework', [
-        'profiler' => ['collect' => false]
+        'profiler' => ['only_exceptions' => false]
     ]);
 };
